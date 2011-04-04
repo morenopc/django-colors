678fbc301a657b602db3ba8bf95c4ce1c7179d85
Ryan <ryan@archweblabs.com>
2011-04-04 16:29:30 -0700

    Fixed indent error

--- colors/fields.py
+++ colors/fields.py
@@ -4,17 +4,15 @@ from django.db import models
 from django import forms
 from django.utils.text import capfirst
 
-
 from colors.widgets import ColorPickerWidget
 from colors.validators import HexColorCodeValidator
 
 class ColorField(models.CharField):
-    def __init__(self, *args, **kwargs):
-        kwargs['max_length'] = 6
+	def __init__(self, *args, **kwargs):
+		kwargs['max_length'] = 6
 		self.validators.append(HexColorCodeValidator())
-        super(ColorField, self).__init__(*args, **kwargs)
-
+		super(ColorField, self).__init__(*args, **kwargs)
     def formfield(self, **kwargs):
-        kwargs['widget'] = ColorPickerWidget(attrs={'autocomplete': 'off'})
-        return super(ColorField, self).formfield(**kwargs)
+		kwargs['widget'] = ColorPickerWidget(attrs={'autocomplete': 'off'})
+		return super(ColorField, self).formfield(**kwargs)
 
