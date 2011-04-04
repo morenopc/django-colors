e42f510f14b968b5022fc08896d98cbf68a5b459
Ryan <ryan@archweblabs.com>
2011-04-04 16:41:10 -0700

    Fixing super problem

--- colors/fields.py
+++ colors/fields.py
@@ -5,16 +5,15 @@ from django import forms
 from django.utils.text import capfirst
 
 from colors.widgets import ColorPickerWidget
-from colors.validators import HexColorCodeValidator
+from colors.validators import hex_color_code_validator
 
 class ColorField(models.CharField):
-	default_validators = [HexColorCodeValidator]
+	default_validators = [hex_color_code_validator]
 	
 	def __init__(self, *args, **kwargs):
 		kwargs['max_length'] = 6
 		super(ColorField, self).__init__(*args, **kwargs)
 		
-	
 	def formfield(self, **kwargs):
 		kwargs['widget'] = ColorPickerWidget(attrs={'autocomplete': 'off'})
 		return super(ColorField, self).formfield(**kwargs)
--- colors/validators.py
+++ colors/validators.py
@@ -3,6 +3,5 @@ import re
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext as _
 
-class HexColorCodeValidator(RegexValidator):
-	regex = re.compile(r'([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})')
-	message = _(u"Enter a valid hex color code.")
+hex_color_code_re = re.compile(r'([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})')
+hex_color_code_validator = RegexValidator(hex_color_code_re, _(u"Enter a valid hex color code."), 'invalid')
