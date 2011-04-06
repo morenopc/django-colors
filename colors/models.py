f360259df7d64ba1721cc7c9bf935a4e709d19fc
Ryan <ryan@archweblabs.com>
2011-04-05 22:20:45 -0700

    Changed unicode to color for Color model

--- colors/models.py
+++ colors/models.py
@@ -8,7 +8,7 @@ class Color(models.Model):
 	A unique color in the color library
 	"""
 	name = models.CharField(max_length=15, help_text="Provide a descriptive name for the color.")
-	color = ColorField(_(u"color code"), unique=True, help_text="Click the input field to open the color picker.")
+	color = ColorField(unique=True, help_text="Click the input field to open the color picker.")
 	#classification = models.CharField(max_length=15, choices=COLOR_NAME_CHOICES, editable=False)
 	
 	def __unicode__(self):
